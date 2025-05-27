eda67355abdbaad5e3fb036f4e2b32457c282e18
Victor <74342819+valcantar1985@users.noreply.github.com>
2025-05-27 10:48:25 -0700

    carousel addition

---
+++ assets/js/scripts.js
@@ -0,0 +1,68 @@
+// Carousel
+$(document).ready(function () {
+    const $carousel = $('#carousel');
+    const $items = $carousel.find('.carousel-item');
+    const $indicators = $carousel.find('.carousel-indicators button');
+    const $next = $carousel.find('.control-next');
+    const $prev = $carousel.find('.control-prev');
+
+    let currentIndex = 0;
+    const totalItems = $items.length;
+    const intervalTime = 5000; // 5 seconds
+    let autoSlideInterval;
+
+    function showItem(index) {
+        $items.removeClass('d-block').addClass('d-none');
+        $items.eq(index).removeClass('d-none').addClass('d-block');
+
+        $indicators.removeClass('active');
+        $indicators.eq(index).addClass('active');
+
+        currentIndex = index;
+    }
+
+    function nextItem() {
+        const nextIndex = (currentIndex + 1) % totalItems;
+        showItem(nextIndex);
+    }
+
+    function prevItem() {
+        const prevIndex = (currentIndex - 1 + totalItems) % totalItems;
+        showItem(prevIndex);
+    }
+
+    function startAutoSlide() {
+        autoSlideInterval = setInterval(nextItem, intervalTime);
+    }
+
+    function stopAutoSlide() {
+        clearInterval(autoSlideInterval);
+    }
+
+    // Init
+    showItem(currentIndex);
+    startAutoSlide();
+
+    // Controls
+    $indicators.each(function (index) {
+        $(this).on('click', function () {
+            stopAutoSlide();
+            showItem(index);
+            startAutoSlide();
+        });
+    });
+
+    $next.on('click', function () {
+        stopAutoSlide();
+        nextItem();
+        startAutoSlide();
+    });
+
+    $prev.on('click', function () {
+        stopAutoSlide();
+        prevItem();
+        startAutoSlide();
+    });
+});
+
+// End Carousel
